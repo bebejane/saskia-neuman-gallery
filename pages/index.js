7432bc813cb96a7ecf54b7d76cf9c6db8dec5b2d
Bebe Jane <bjornthief@gmail.com>
2022-04-12 21:59:23 +0200

    use dato colors

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,8 @@ export default function Start({start, image, color}){
 	const scrollPercentage = (scrollY/height);
 	const category = current._modelApiKey === 'show' ? 'SHOWING NOW' : 'UPCOMING'
 	const title = `${current.title} ${current.artists ? current.artists[0].name : ''}`
-	const backgroundColor = color
-
+	const backgroundColor = `rgb(${image?.colors[0].red},${image?.colors[0].green},${image?.colors[0].blue})`
+	
 	const logoStyle = {background:`url(${image.url}?w=400)`}
 	const colorStyle = {
 		backgroundColor, 
@@ -32,17 +32,18 @@ export default function Start({start, image, color}){
 		top:`${(scrollPercentage*height)*2}px`, 
 		maxHeight:`${100-(scrollPercentage*100)}vh`,
 	};
+
+	useEffect(()=>{
+		document.body.style.backgroundColor = backgroundColor;
+		return () => document.body.style.backgroundColor = 'unset';
+	},[])
 	
 	return (
 		<div className={styles.container}>
-			{image && !animating && 
-				<Image lazyLoad={false} className={`${styles.backgroundImage} ${styles.show}`} data={image.responsiveImage}/>
-			}
+			{image && !animating && <Image lazyLoad={false} className={`${styles.backgroundImage} ${styles.show}`} data={image.responsiveImage}/>}
 			<div className={cn(styles.color, styles.lower)} style={colorStyleLower}></div>
 			<div className={cn(styles.logo)} style={logoStyle}><h1>SASKIA NEUMAN GALLERY</h1></div>
 			<div className={cn(styles.color, styles.upper)} style={colorStyle} onAnimationEnd={()=>setAnimating(false)}></div>
-			
-			
 			<div className={cn(styles.titleContainer)}>
 				<Link href={`/${current._modelApiKey}s/${current.slug}`}>
 					<a>
@@ -59,17 +60,16 @@ export default function Start({start, image, color}){
 
 export const getStaticProps = withGlobalProps({queries:[GetStart], model:'start'}, async ({props, revalidate }) => {
 	const { current } = props.start
-
-	const image = current.image ? current.image : current.images?.length ? current.images[0] : null
-	const palette = await Vibrant.from(image.url).getPalette()
-	const dominantColor = Object.keys(palette).map(k => palette[k]).sort((a,b) => a._population > b._population ? 1 : -1)[0]._rgb
-	const color = image ? `rgb(${dominantColor.join(',')})` : null;
-
+	const image = current?.image ? current.image : current?.images?.length ? current?.images[0] : null
+	//const palette = await Vibrant.from(image.url).getPalette()
+	//const dominantColor = palette.DarkMuted._rgb// Object.keys(palette).map(k => palette[k]).sort((a,b) => a._population < b._population ? 1 : -1)[0]._rgb
+	//const color = image ? `rgb(${dominantColor.join(',')})` : null;
+	//console.log(palette)
 	return {
 		props:{
 			...props,
-			image, 
-			color
+			image
+			//color
 		},
 		revalidate
 	};
