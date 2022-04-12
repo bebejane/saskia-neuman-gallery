b158320472ced6673125bda302c26e4d9f26d9e1
Bebe Jane <bjornthief@gmail.com>
2022-04-12 16:13:00 +0200

    fix splash. color thief

--- pages/index.js
+++ pages/index.js
@@ -5,30 +5,41 @@ import { Image } from "react-datocms"
 import cn from "classnames"
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useWindowSize, useWindowScrollPosition } from 'rooks';
 
-export default function Start({start}){
-	//console.log(start)
-	const { current } = start
-	
+const { getColorFromURL } = require('color-thief-node');
+
+export default function Start({current, image, color}){
 	if(!current) return null;
 
 	const [animating, setAnimating] = useState(true)
-	const image = current.image ? current.image : current.images?.length ? current.images[0] : null
-	
-	const category = current._modelApiKey === 'show' ? 'SHOWING NOW' : 'UPCOMING'
+	const { scrollY } = useWindowScrollPosition()
+	const { innerHeight: height, innerWidth:width} = useWindowSize()
 
+	const scrollPercentage = (scrollY/height);
+	const category = current._modelApiKey === 'show' ? 'SHOWING NOW' : 'UPCOMING'
 	const title = `${current.title} ${current.artists ? current.artists[0].name : ''}`
-	const color = 'rgb(255, 207, 188)'
-	
+	const backgroundColor = color
+	const logoStyle = {background:`url(${image.url})`}
+	const colorStyle = {backgroundColor, top:`${(scrollPercentage*height)*2}px`, maxHeight:`${((1-(scrollPercentage))*100)}vh`};
+
+	useEffect(()=>{
+		
+		
+		//const {clientWidth} = document.querySelector(`.${styles.backgroundImage} > picture > img`)
+		//onsole.log(image)
+		//console.log(clientWidth)
+	}, [image, animating])
+	//console.log(size)
 	return (
 		<div className={styles.container}>
-			{image && !animating && <Image className={`${styles.backgroundImage} ${styles.show}`} data={image.responsiveImage}/>}
-			
-			{/* */}
-			<div className={cn(styles.logo)} onAnimationEnd={()=>setAnimating(false)}>
+			{image && !animating && 
+				<Image className={`${styles.backgroundImage} ${styles.show}`} data={image.responsiveImage}/>
+			}
+			<div className={cn(styles.logo)} style={logoStyle} onAnimationEnd={()=>setAnimating(false)} >
 				{!animating && <h1>SASKIA NEUMAN GALLERY</h1>}
 			</div>
-			<div className={cn(styles.color)} style={{backgroundColor:color}}></div>
+			<div className={cn(styles.color)} style={colorStyle}></div>
 			{!animating &&
 				<div className={cn(styles.titleContainer)}>
 					<Link href={`/${current._modelApiKey}s/${current.slug}`}>
@@ -45,9 +56,17 @@ export default function Start({start}){
 	)
 }
 
-export const getStaticProps = withGlobalProps({queries:[GetStart]}, async ({props, revalidate }) => {
+export const getStaticProps = withGlobalProps({queries:[GetStart], model:'start'}, async ({props, revalidate }) => {
+	const { current } = props.start
+	const image = current.image ? current.image : current.images?.length ? current.images[0] : null
+	const color = image ? `rgb(${(await getColorFromURL(image.url)).join(',')})` : null;
+	
 	return {
-		props,
+		props:{
+			...props.start,
+			image, 
+			color
+		},
 		revalidate
 	};
 });
