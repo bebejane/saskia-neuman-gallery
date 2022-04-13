767f9aef118b71b0e89fd125a516f49548e55809
Bebe Jane <bjornthief@gmail.com>
2022-04-13 10:50:35 +0200

    fix revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -6,25 +6,34 @@ export default async (req, res) => {
   const { entity } = req.body
   
   try{
-    
+    let path;
     const models = await Dato.itemTypes.all();
     const modelId = entity.relationships.item_type.data.id
     const model = models.filter(m => m.id === modelId)[0]
-    const record = await Dato.items.all({filter: {id: entity.id }},{allPages: true})
-    let path;
-    
+    const record = (await Dato.items.all({filter: {type: model.apiKey, fields:{id: {eq:entity.id }}}},{allPages: true}))[0]
+
+    if(!record) 
+      throw new Error(`Error revalidating! Record not found with id ${entity.id}`);
+
     switch (model.apiKey) {
+      case "":
+        path = `/`
+      case "about":
+        path = `/about`
       case "show":
         path = `/shows/${record.slug}`
       case "event":
         path = `/events/${record.slug}`
       case "artist":
         path = `/artists/${record.slug}`
+      case "start":
+        path = `/`
       default:
         break;
     }
 
-    if(!path) throw new Error('Error revalidating! ' + model.apiKey);
+    if(!path) 
+      throw new Error(`Error revalidating! Path not found for model ${model.apiKey}`);
 
     await res.unstable_revalidate(path)
     res.json({ revalidated: true, path })
