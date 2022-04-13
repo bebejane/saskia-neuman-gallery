c23984d0c9506fddf290f9e7f4f1914773a68802
Bebe Jane <bjornthief@gmail.com>
2022-04-13 10:25:46 +0200

    try again

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -12,12 +12,12 @@ export default async (req, res) => {
     const model = models.filter(m => m.id === modelId)[0]
     const record = await Dato.items.all({filter: {id: entity.id }},{allPages: true})
     const path = ['show', 'event', 'artist'].includes(model.apiKey) ? `/${model.apiKey}s/${record.slug}` : null
-    if(!path) return res.status(500).send('Error revalidating! ' + model.apiKey)
+    if(!path) throw new Error('Error revalidating! ' + model.apiKey);
+
     await res.unstable_revalidate(path)
     res.json({ revalidated: true, path })
   }catch(err){
-    res.json(err)
+    console.log(err)
+    res.status(500).send('Error revalidating! ' + err.message)
   }
-  
-
 }
