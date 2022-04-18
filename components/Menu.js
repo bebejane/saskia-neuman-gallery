09c6e97d2e8a6c50b66bf4ae625ab3c6cf2ba290
Bebe Jane <bjornthief@gmail.com>
2022-04-18 18:33:33 +0200

    mobile menu

--- components/Menu.js
+++ components/Menu.js
@@ -25,7 +25,8 @@ export default function Menu({artists, shows, events}){
   const { scrollDirection, isScrolling, isScrollingUp, isScrollingDown} = useScrollDirection();
 
   const showSeparator = subMenu && menu.filter(({sub, type}) => type === subMenu?.type).length
-  
+  const menuStyles = cn(styles.container, (subMenu || showMobileMenu) && styles.open, (!showMenu && ! showMobileMenu) && styles.hide)
+
   useEffect(()=>{
     const handleRouteChange = (url, { shallow }) => {
       setShowMobileMenu(false)
@@ -50,24 +51,58 @@ export default function Menu({artists, shows, events}){
 	
   return (
     <>
-      <div className={styles.logo}>
+      <div className={cn(styles.top, !showMenu && styles.hide)}>
         <Link href="/">SASKIA NEUMAN GALLERY</Link>
+        <div className={styles.hamburger}>
+          <Hamburger
+            toggled={showMobileMenu}
+            duration={0.5}
+            onToggle={(toggle) => setShowMobileMenu(toggle)}
+            color={'#000'}
+            label={'Menu'}
+            size={20}
+          />
+        </div>
       </div>
-      <div id="menu" className={cn(styles.container, (subMenu || showMobileMenu) && showSeparator && styles.open, !showMenu && styles.hide)} onMouseLeave={()=>setSubMenu()}>
+      <div 
+        id="menu" 
+        className={menuStyles} 
+        onMouseLeave={()=>setSubMenu()}
+      >
         <div className={cn(styles.menu)}>
           <ul>
             {menu.map((m, idx) => 
-              <li id={`menu-${m.type}`} key={idx} className={cn(router.asPath === m.path && styles.selected)} onMouseOver={()=> setSubMenu(m)}>
+              <li 
+                id={`menu-${m.type}`} 
+                key={idx} 
+                className={cn(router.asPath === m.path && styles.selected)} 
+                onMouseOver={()=> setSubMenu(m)
+              }>
                 {m.sub  ? 
-                  <a onClick={()=> setSubMenu(subMenu ? null : m)}>{m.label}</a>
+                  <a onClick={()=> setSubMenu(m)}>{m.label}</a>
                 : 
                   <Link href={m.path}><a>{m.label}</a></Link> 
+                }
+                {showMobileMenu && m.type === subMenu?.type &&
+                  <ul 
+                    key={idx}
+                    id={`sub-${m.type}`} 
+                    className={cn(subMenu?.type === m.type && styles.open)} 
+                  >
+                    {m.sub?.map((a, idx) => 
+                      <Link href={`${m.path}/${a.slug}`}>
+                        <li key={idx}>
+                          <a>{a.name || a.title}</a>
+                        </li>
+                      </Link>
+                    )}
+                  </ul>
                 }
               </li>
             )}
           </ul>
           <div className={cn(styles.subMenu)}>
-            {menu.map(({type, path, label, sub}, idx) => (sub) &&
+            {menu.map(({type, path, label, sub}, idx) => sub && !showMobileMenu &&
               <ul 
                 key={idx}
                 id={`sub-${type}`} 
@@ -84,18 +119,13 @@ export default function Menu({artists, shows, events}){
               </ul>
             )}
           </div>
-          <div className={cn(styles.line, showSeparator && styles.show)} style={{marginLeft:`${subMenuMargin-0}px`}}></div>
+          <div 
+            className={cn(styles.separator, showSeparator && styles.show)} 
+            style={{marginLeft:`${subMenuMargin-0}px`}}
+          ></div>
         </div>
-
-        <Hamburger
-          toggled={showMobileMenu}
-          duration={0.5}
-          onToggle={(toggle) => setShowMobileMenu(toggle)}
-          color={'#000'}
-          label={'Menu'}
-          size={20}
-        />
       </div>
+      
     </>
   )
 }
