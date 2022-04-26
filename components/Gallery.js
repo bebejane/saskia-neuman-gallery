0ba8445ca42190f7bbe717e398ebeceef62d4292
konst-teknik <information-konst-teknik.se>
2022-04-26 14:50:16 +0200

    Gallery osv

--- components/Gallery.js
+++ components/Gallery.js
@@ -3,30 +3,32 @@ import { Image } from "react-datocms"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState, useRef } from 'react';
 
-export default function Gallery({images, onClose, index = 0}){
-  if(!images) return null;
+export default function Gallery({ images, onClose, index = 0 }) {
+  if (!images) return null;
   const swiperRef = useRef()
 
   return (
-		<div className={styles.gallery}>
-      <div className={styles.images} onClick={()=> swiperRef?.current?.slideNext()}>
+    <div className={styles.gallery}>
+      <div className={styles.images} onClick={() => swiperRef?.current?.slideNext()}>
         <Swiper
           loop={true}
+          centerSlided={true}
           spaceBetween={50}
           slidesPerView={1}
           effect={'cube'}
           initialSlide={index}
           onSwiper={(swiper) => swiperRef.current = swiper}
         >
-        {images.map((image, idx) => 
-          <SwiperSlide key={idx} className={styles.slide}>
-            <Image data={image.responsiveImage}/>
-            <div className={styles.label}>{image.title}</div>
-          </SwiperSlide>
-        )}
+          {images.map((image, idx) =>
+            <SwiperSlide key={idx} className={styles.slide}>
+              <Image data={image.responsiveImage} />
+              <div className={styles.label}>{image.title}</div>
+            </SwiperSlide>
+          )}
         </Swiper>
       </div>
       <div className={styles.close} onClick={onClose}>CLOSE</div>
-		</div>
-	)
+      <div className={styles.caption}>Här kommer bildtext</div>
+    </div>
+  )
 }
--- pages/artists/[...slug].js
+++ pages/artists/[...slug].js
@@ -2,57 +2,67 @@ import styles from './Artists.module.scss'
 import { apiQuery } from '/lib/dato/api';
 import { withGlobalProps } from "/lib/hoc";
 import { imageColor, imageBrightness } from '/lib/utils';
-import { GetAllArtists,  GetArtist } from '/graphql';
+import { GetAllArtists, GetArtist } from '/graphql';
 import { Image } from 'react-datocms';
 import Markdown from '/lib/dato/components/Markdown';
 import Link from 'next/link';
 import Gallery from '/components/Gallery'
 import { useState } from 'react';
+import { Layout, Meta, Content } from '/components/Layout'
+import { HeaderBar } from 'components/HeaderBar';
+import GalleryThumbs from 'components/GalleryThumbs';
 
-export default function Artist({artist:{ name, biography, artwork }}){
+export default function Artist({ artist: { name, biography, artwork } }) {
 	const [galleryIndex, setGalleryIndex] = useState()
 
 	return (
 		<>
-			<main>
-				{name}<br/>
-				<Markdown>{biography}</Markdown>
-				<div className={styles.gallery}>
-					{artwork.map((image, idx) => 
-						<div key={idx} className={styles.artwork} onClick={()=>setGalleryIndex(idx)}>
-							<Image data={image.responsiveImage} className={styles.artwork} />
-						</div>
-					)}
-				</div>
-				<a onClick={()=>setShowGallery(true)}>[Gallery]</a>
-			</main>
-			{galleryIndex !== undefined && 
-				<Gallery images={artwork} index={galleryIndex} onClose={()=>setGalleryIndex(undefined)}/>
+			<Layout>
+				<Meta>
+					<HeaderBar>
+						<h3>ARTISTS</h3>
+					</HeaderBar>
+				</Meta>
+
+				<Content>
+					<HeaderBar><h1>{name}</h1></HeaderBar>
+					<Markdown>{biography}</Markdown>
+					<div className={styles.gallery}>
+						{artwork.map((image, idx) =>
+							<div key={idx} className={styles.artwork} onClick={() => setGalleryIndex(idx)}>
+								<Image data={image.responsiveImage} className={styles.artwork} />
+							</div>
+						)}
+					</div>
+				</Content>
+			</Layout>
+			{galleryIndex !== undefined &&
+				<Gallery images={artwork} index={galleryIndex} onClose={() => setGalleryIndex(undefined)} />
 			}
 		</>
 	)
 }
 
 export async function getStaticPaths(context) {
-  const { artists } = await apiQuery(GetAllArtists)
-	const paths = artists.map(({slug}) => ({params:{slug:[slug]}}))
+	const { artists } = await apiQuery(GetAllArtists)
+	const paths = artists.map(({ slug }) => ({ params: { slug: [slug] } }))
 	return {
 		paths,
 		fallback: 'blocking'
 	}
 }
 
-export const getStaticProps = withGlobalProps({model:'artist'}, async ({props, context, revalidate }) => {
-  const { artist } = await apiQuery(GetArtist, {slug:context.params.slug[0]})
-  
+export const getStaticProps = withGlobalProps({ model: 'artist' }, async ({ props, context, revalidate }) => {
+	const { artist } = await apiQuery(GetArtist, { slug: context.params.slug[0] })
+
 	return {
-		props :{
-      ...props,
-      artist,
+		props: {
+			...props,
+			artist,
 			image: artist.image || null,
 			color: imageColor(artist.image),
-			brightness : await imageBrightness(artist.image),
-    },
+			brightness: await imageBrightness(artist.image),
+		},
 		revalidate
 	};
 });
