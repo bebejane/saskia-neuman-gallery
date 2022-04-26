495bbfa4d7a02cef7556bc31f033f26ccee74f0e
Bebe Jane <bjornthief@gmail.com>
2022-04-26 13:38:59 +0200

    gallery popup

--- components/GalleryThumbs.js
+++ components/GalleryThumbs.js
@@ -1,12 +1,24 @@
 import styles from './GalleryThumbs.module.scss'
 import { Image } from "react-datocms"
+import { useState } from 'react'
+import Gallery from './Gallery'
 
 export default function GalleryThumbs({ artwork }) {
+  
+  const [galleryIndex, setGalleryIndex] = useState()
+
   return (
-    <div className={styles.galleryThumbs}>
-      {artwork.map((image, idx) =>
-        <figure><Image key={idx} onClick={() => setGalleryIndex(idx)} data={image.responsiveImage} /></figure>
-      )}
-    </div>
+    <>
+      <div className={styles.galleryThumbs}>
+        {artwork.map((image, idx) =>
+          <figure onClick={() => setGalleryIndex(idx)}>
+            <Image key={idx} data={image.responsiveImage} />
+          </figure>
+        )}
+      </div>
+      {galleryIndex !== undefined && 
+				<Gallery images={artwork} index={galleryIndex} onClose={()=>setGalleryIndex(undefined)}/>
+			}
+    </>
   )
 }
