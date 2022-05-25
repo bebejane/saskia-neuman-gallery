b521519a8301bc2bbb41d398df7440dec0d2cf9a
Bebe Jane <bjornthief@gmail.com>
2022-05-25 11:59:07 +0200

    test trans

--- components/PageTransition.js
+++ components/PageTransition.js
@@ -95,7 +95,7 @@ export default function PageTransition({image}){
       <div className={styles.color} style={{backgroundColor: color}}>
         <div 
 					className={cn(styles.logo)}
-					style={{background:`url(${image?.url}?w=1400)`}}
+					//style={{background:`url(${image?.url}?w=1400)`}}
 				>
           <h1 style={{opacity: showLogo ? 1 : 0}}>
 						SASKIA NEUMAN GALLERY
