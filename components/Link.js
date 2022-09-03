1d803f6d2e7c9d9327efbcdefb2e0ded411b78c6
Bebe Jane <bjornthief@gmail.com>
2022-09-03 12:29:05 +0200

    fix white on white menu hover/selected

--- components/Link.js
+++ components/Link.js
@@ -22,9 +22,11 @@ export function Link({
   const router = useRouter()
   const [hover, setHover] = useState(false)
   const setBackgroundColor = useStore((state) => state.setBackgroundColor)
-  const linkRef = useRef() 
-  const linkStyle = color && (hover || isSelected) ? { color: `rgb(${color.join(',')})`, textShadow: '0 0 5px #fff05' } : {}
+  const linkRef = useRef()
+  const isWhite = color?.reduce((prev, curr) => parseInt(prev)+parseInt(curr), 0) >= ((255*3)*0.97)
 
+  const linkStyle = color && (hover || isSelected) ? { color: isWhite ? 'rgb(0,0,0)' : `rgb(${color.join(',')})`, textShadow: '0 0 5px #fff05' } : {}
+  
   const handleMouse = (e) => {
     if (e.type === 'mouseleave') {
       setHover(false)
