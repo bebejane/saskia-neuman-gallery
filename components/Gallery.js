e034676e1c142ffdbb023e4210b0e3e136ccf0e4
Bebe Jane <bjornthief@gmail.com>
2022-11-08 15:41:01 +0100

    show blurup in gallery

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,54 +1,61 @@
 import "swiper/css";
-import styles from './Gallery.module.scss'
-import { Image } from "react-datocms"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { useState, useRef, useEffect } from 'react';
+import styles from "./Gallery.module.scss";
+import { Image } from "react-datocms";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState, useRef, useEffect } from "react";
 
 export default function Gallery({ show, images, onClose, index = 0 }) {
+	const swiperRef = useRef();
+	const [realIndex, setRealIndex] = useState(index);
+	const [caption, setCaption] = useState({});
 
-  const swiperRef = useRef()
-  const [realIndex, setRealIndex] = useState(index)
-  const [caption, setCaption] = useState({})
+	useEffect(() => {
+		if (!images || !images[realIndex]) return;
+		const { title, alt } = images[realIndex];
+		setCaption({ title, alt });
+	}, [images, realIndex]);
 
-  useEffect(() => {
-    if(!images || !images[realIndex]) return
-    const { title, alt } = images[realIndex]
-    setCaption({ title, alt })
-  }, [images, realIndex])
+	if (!images || !show) return null;
 
-  if (!images || !show) return null
-
-  return (
-    <div className={styles.gallery}>
-      <div className={styles.back} onClick={() => swiperRef.current.slidePrev()}>❮</div>
-      <div className={styles.images} onClick={() => swiperRef?.current?.slideNext()}>
-        <Swiper
-          loop={true}
-          spaceBetween={500}
-          slidesPerView={1}
-          initialSlide={index}
-          onSlideChange={({ realIndex }) => setRealIndex(realIndex)}
-          onSwiper={(swiper) => swiperRef.current = swiper}
-        >
-          {images.map((image, idx) =>
-            <SwiperSlide key={idx} className={styles.slide}>
-              {<Image
-                className={styles.image}
-                pictureClassName={styles.picture}
-                data={image.responsiveImage}
-                lazyLoad={false}
-                usePlaceholder={false}
-              />}
-            </SwiperSlide>
-          )}
-        </Swiper>
-      </div>
-      <div className={styles.forward} onClick={() => swiperRef.current.slideNext()}>❯</div>
-      <div className={styles.caption}>
-        <span>{caption.title}</span>
-        {caption.alt && <span className={styles.subTitle}>{caption.alt}</span>}
-      </div>
-      <div className={styles.close} onClick={onClose}>×</div>
-    </div>
-  )
+	return (
+		<div className={styles.gallery}>
+			<div className={styles.back} onClick={() => swiperRef.current.slidePrev()}>
+				❮
+			</div>
+			<div className={styles.images} onClick={() => swiperRef?.current?.slideNext()}>
+				<Swiper
+					loop={true}
+					spaceBetween={500}
+					slidesPerView={1}
+					initialSlide={index}
+					onSlideChange={({ realIndex }) => setRealIndex(realIndex)}
+					onSwiper={(swiper) => (swiperRef.current = swiper)}
+				>
+					{images.map((image, idx) => (
+						<SwiperSlide key={idx} className={styles.slide}>
+							{
+								<Image
+									className={styles.image}
+									pictureClassName={styles.picture}
+									data={image.responsiveImage}
+									lazyLoad={false}
+									usePlaceholder={true}
+								/>
+							}
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+			<div className={styles.forward} onClick={() => swiperRef.current.slideNext()}>
+				❯
+			</div>
+			<div className={styles.caption}>
+				<span>{caption.title}</span>
+				{caption.alt && <span className={styles.subTitle}>{caption.alt}</span>}
+			</div>
+			<div className={styles.close} onClick={onClose}>
+				×
+			</div>
+		</div>
+	);
 }
