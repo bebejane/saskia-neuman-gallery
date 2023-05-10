cc9efc4316722d31ea8afbb4a15fa9305fa31791
Bebe Jane <bjornthief@gmail.com>
2023-05-10 18:01:58 +0200

    web previews

---
+++ pages/api/web-previews.js
@@ -0,0 +1,30 @@
+import { withWebPreviews } from "dato-nextjs-utils/hoc";
+
+export default withWebPreviews(async ({ item, itemType }) => {
+	let path = null;
+	const { slug } = item.attributes;
+	const { api_key } = itemType.attributes;
+
+	switch (api_key) {
+		case "start":
+			path = `/`;
+			break;
+		case "about":
+		case "external_link":
+			path = `/about`;
+			break;
+		case "exhibition":
+			path = `/exhibitions/${slug}`;
+			break;
+		case "happening":
+			path = `/happenings/${slug}`;
+			break;
+		case "artist":
+			path = `/artists/${slug}`;
+			break;
+		default:
+			break;
+	}
+
+	return path;
+});
